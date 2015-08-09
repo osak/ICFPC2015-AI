003429948273938818a156729bbea7e6ae5ff559
Masata Kida <masata@indeed.com>
2015-08-09 02:44:48 -0500

    first buddha

--- ai/evaluation/lightningeval.cpp
+++ ai/evaluation/lightningeval.cpp
@@ -57,10 +57,34 @@ int LightningEval::chanceScore(const vector <BitRow> &f, int leftTurn) {
 }
 
 int LightningEval::calc(vector <BitRow> &field, int num){
-	if (num == units.size()) return 0;
+	/*if (num == units.size()) return 0;
 	Unit &next = units[num];
 	if (!Util::check(H, W, field, next.pivot, 0, next)) return -1e9;
 	if (maxUnitSize == 1) return oneUnitScore(field);
 	return kawateaScore(field) + heightScore(field) + chanceScore(field, units.size() - num) + dangerScore(field);
+	*/
+	int val = 0;
+	int searchSize = 4;
+	for (int y = 0; y < H; y++) {
+		for (int x = 0; x < W; x++) {
+			if (!field[y].get(x)) {
+				int s = 0;
+				for (int yy = y - 1; y - yy <= searchSize; yy--) {
+					if (yy < 0) {
+						s += 100 * (y - yy + 1) / (y - yy);
+					} else {
+						for (int xx = x - yy/2 - ((yy&1) && !(y&1)); xx <= x + yy/2 + ((yy&1) && (y&1)); xx++) {
+							if (xx < 0 || xx >= W) {
+								s += 100 / (y - yy);
+							} else {
+								s += 100 * field[yy].get(xx) / (y - yy);
+							}
+						}
+					}
+				}
+				val -= s * field[y].popcount();
+			}
+		}
+	}
+	return val + heightScore(field) * 100 * W * H;
 }
-
