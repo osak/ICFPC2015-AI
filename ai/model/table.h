51a3c5c3d8b5cc615b9786f8af3a9590edd54554
Tomoyuki Hazehata <tomoyuki@indeed.com>
2015-08-09 11:58:13 -0500

    osamupower2

---
+++ ai/model/table.h
@@ -0,0 +1,49 @@
+//
+// Created by yaya on 8/9/15.
+//
+
+#ifndef AI_TABLE_H
+#define AI_TABLE_H
+
+#include "../common.h"
+#include "unit.h"
+
+const int MAX_BOARD_SIZE = 300;
+class Table {
+    std::pair<int, int> table[MAX_BOARD_SIZE+1][MAX_BOARD_SIZE+1][6];
+    int turn;
+
+public:
+    Table() {
+        for(int i = 0; i < MAX_BOARD_SIZE; ++i) {
+            for(int j = 0; j < MAX_BOARD_SIZE; ++j) {
+                for (int k = 0; k < 6; ++k) {
+                    table[i][j][k] = make_pair(-1, -1);
+                }
+            }
+        }
+        turn = 0;
+    }
+
+    int operator[](const std::pair<Point, int> &key) const {
+        // Here assumes given key is **valid** one; must be already inserted.
+        return table[key.first.x+1][key.first.y+1][key.second].second;
+    }
+
+    int& operator[](const std::pair<Point, int> &key) {
+        // Here assumes value always to be set when caller requires lvalue.
+        table[key.first.x+1][key.first.y+1][key.second].first = turn;
+        return table[key.first.x+1][key.first.y+1][key.second].second;
+    }
+
+    bool count(const std::pair<Point, int> &key) const {
+        return table[key.first.x+1][key.first.y+1][key.second].first == turn;
+    }
+
+    void clear() {
+        ++turn;
+    }
+};
+
+
+#endif //AI_TABLE_H
